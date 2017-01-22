ce8a6d1d1867a41ef9524aaa089405f1429c35ca
Olivier Bourdoux <olivier.bourdoux@gmail.com>
2017-01-22 19:55:17 +0100

    Removed lsDir

--- index.js
+++ index.js
@@ -2,9 +2,3 @@ module.exports = {
 	isset: require('./isset'),
 	arrayToMap: require('./array-to-map'),
 };
-
-if (!require('is-browser')) {
-	module.exports = Object.assign(module.exports, {
-		lsDir: require('./ls-dir'),
-	});
-}
